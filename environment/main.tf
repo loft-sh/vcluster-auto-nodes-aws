71c1adb470e2c1c91dbbd8225034de6ef558158e
Bill DeCoste <bdecoste@gmail.com>
2025-11-14 09:28:14 -0600

    update

--- environment/main.tf
+++ environment/main.tf
@@ -11,22 +11,3 @@ module "validation" {
   region = var.vcluster.requirements["region"]
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 6.0"
-
-  name = local.vpc_name
-  cidr = local.vpc_cidr_block
-
-  azs                    = local.azs
-  private_subnets        = local.private_subnets
-  public_subnets         = local.public_subnets
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
-
-  tags = {
-    Name = local.vpc_name
-  }
-}
-
--- node/locals.tf
+++ node/locals.tf
@@ -2,8 +2,8 @@ locals {
   vcluster_name      = nonsensitive(var.vcluster.instance.metadata.name)
   vcluster_namespace = nonsensitive(var.vcluster.instance.metadata.namespace)
 
-  subnet_id         = var.vcluster.nodeEnvironment.outputs["private_subnet_ids"][random_integer.subnet_index.result]
+  subnet_id         = "subnet-0de0bead18530989b" 
   instance_type     = var.vcluster.nodeType.spec.properties["instance-type"]
   user_data         = var.vcluster.userData
-  security_group_id = var.vcluster.nodeEnvironment.outputs["security_group_id"]
+  security_group_id = "sg-025366b7fd8a56586" 
 }
