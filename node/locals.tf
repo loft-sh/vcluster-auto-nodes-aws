a81c705452bdf6d23cd3359bb1efed003b6f6628
Bill DeCoste <bdecoste@gmail.com>
2025-11-14 13:02:14 -0600

    update

--- node/locals.tf
+++ node/locals.tf
@@ -2,7 +2,7 @@ locals {
   vcluster_name      = nonsensitive(var.vcluster.instance.metadata.name)
   vcluster_namespace = nonsensitive(var.vcluster.instance.metadata.namespace)
 
-  subnet_id         = "subnet-0de0bead18530989b" 
+  subnet_id         = "subnet-05e249adfbcfecf31" 
   instance_type     = var.vcluster.nodeType.spec.properties["instance-type"]
   user_data         = var.vcluster.userData
   security_group_id = "sg-025366b7fd8a56586" 
